2eeb9479ab2a969d19f51de70abb8fb8e6c3eb2c
sean0921 <sean.li.shin.ho@gmail.com>
2019-04-10 04:26:51 +0800

    still follow the way to generate another script and run it
    still no python dependency, still figuring out better way

--- SAC_wf_proc_hp.bash
+++ SAC_wf_proc_hp.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 # show commands we are runnning, and if there's any error, abort this script.
 set -eux
@@ -8,17 +8,25 @@ filt_type='hp'   # type of filter
 f_min="1"        # low bound
 nc="2"           # number of poles
 tape_rate="0.02" #apply taper 0.02%
-name_tail='.hp'  # appended text
+name_tail='.hp_sh'  # appended text
 
-work_path="$HOME/data/14241254.P19_2"
+event_path="$HOME/data/week05_events"
 
-printf "working on: $work_path \n"
-
-for i in $(ls $work_path/*.sac)
-do sac <<END
-r $i
-rmean;rtr;taper width $tape_rate
-$filt_type n $nc c $f_min
-w append $name_tail q
-END
+cd $event_path
+for j in $(ls -d */)
+do cd $j
+    for i in $(ls *.sac)
+    do
+    echo "sac <<END" >> run.bash
+    echo "r $i" >> run.bash
+    echo "rmean;rtr;taper width $tape_rate" >> run.bash
+    echo "$filt_type n $nc c $f_min" >> run.bash
+    echo "w append $name_tail" >> run.bash
+    echo "quit" >> run.bash
+    echo "END" >> run.bash
+    /usr/bin/env bash run.bash
+    rm run.bash
+    done
+    cd -
+done
 
